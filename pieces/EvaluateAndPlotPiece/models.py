9b6f73ec9223b0a553d0ceb37e3fd7d3692e8396
StanoVo <stano.vojtko@gmail.com>
2025-11-13 22:03:44 +0100

    EvaluateAndPlotPiece update

--- pieces/EvaluateAndPlotPiece/models.py
+++ pieces/EvaluateAndPlotPiece/models.py
@@ -1,14 +1,39 @@
-﻿from pydantic import BaseModel
+﻿from pydantic import BaseModel, Field
 
 class InputModel(BaseModel):
-    data_path: str
-    model_path: str
-    metrics_out: str
-    plot_out: str
+    data_path: str = Field(
+        title="Path to data file",
+        default='/home/shared_storage/data/DataForPrediction.csv',
+        description="The path to data file for prediction"
+    ) 
+    model_path: str = Field(
+        title="Path to model file",
+        default='/home/shared_storage/data/model.pkl',
+        description="The path to prediction model file"
+    ) 
+    metrics_out: str = Field(
+        title="Path to metrics output file",
+        default='/home/shared_storage/data/metrics.json',
+        description="The path to metrics output file"
+    ) 
+    plot_out: str = Field(
+        title="Path to graphical prediction output file",
+        default='/home/shared_storage/data/comparison.png',
+        description="The path to graphical prediction output file"
+    ) 
 
 class OutputModel(BaseModel):
-    message: str
-    metrics_file: str
-    plot_file: str
+    message: str= Field(
+        default="",
+        description="Output message to log"
+    )
+    metrics_file: str= Field(
+        default="",
+        description="The path to the metrics json file"
+    )
+    plot_file: str= Field(
+        default="",
+        description="The path to the graphical prediction output file"
+    )
     mae: float
     r2: float
