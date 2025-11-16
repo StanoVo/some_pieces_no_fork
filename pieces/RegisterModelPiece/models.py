864f1d228bd33b4e52ab6b2d360aa4d452a58380
StanoVo <stano.vojtko@gmail.com>
2025-11-16 20:32:54 +0100

    Model-Version libs

--- pieces/RegisterModelPiece/models.py
+++ pieces/RegisterModelPiece/models.py
@@ -1,13 +1,35 @@
-﻿from pydantic import BaseModel
+﻿from pydantic import BaseModel, Field
 from typing import Optional
 
 class InputModel(BaseModel):
-    model_path: str
-    metrics_path: str
-    name: str
-    description: Optional[str] = ""
-
+    model_path: str = Field(
+        title="Path to model file",
+        default='/home/shared_storage/data/model.pkl',
+        description="The path to forecast model file"
+    ) 
+    metrics_path: str = Field(
+        title="Path to metrics file",
+        default='/home/shared_storage/data/metrics.json',
+        description="The path to metrics file"
+    )
+    name: str = Field(
+        title="Model name", 
+        default="SolarForecastModel"
+    ) 
+    description: Optional[str] = Field(
+        title="Model description",
+        default="XGBoost model for solar forecasting"
+    )
 class OutputModel(BaseModel):
-    message: str
-    model_version_id: str
-    registry_url: str
+    message: str = Field(
+        default="",
+        description="Output message to log"
+    )
+    model_version_id: str = Field(
+        default="",
+        description="Version ID of the registered model"
+    )
+    registry_url: str = Field(
+        default="",
+        description="The URL to the model registry entry"
+    )
