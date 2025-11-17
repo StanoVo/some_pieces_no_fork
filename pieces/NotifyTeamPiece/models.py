014dbcc9347a0d54ce00e157321c1990049e57d2
StanoVo <stano.vojtko@gmail.com>
2025-11-17 13:19:44 +0100

    NotifyTeamamPiece: Update models.py to add Field metadata and modify config.toml version

--- pieces/NotifyTeamPiece/models.py
+++ pieces/NotifyTeamPiece/models.py
@@ -1,11 +1,34 @@
-﻿from pydantic import BaseModel
+﻿from pydantic import BaseModel, Field
 from typing import Optional
 
 class InputModel(BaseModel):
-    metrics_path: str
-    webhook_url: Optional[str] = None
+    metrics_path: str = Field(
+        title="Path to metrics file",
+        default='/home/shared_storage/data/metrics.json',
+        description="The path to metrics file"
+    )
+    webhook_url: Optional[str] = Field(
+        title="Path to webhook url",
+        default='',
+        description="The path to webhook url for Slack notifications"
+    )
 
 class OutputModel(BaseModel):
-    message: str
+    message: str= Field(
+        default="",
+        description="Output message to log"
+    )
     notification_sent: bool
     recipients: str
+
+# from pydantic import BaseModel
+# from typing import Optional
+
+# class InputModel(BaseModel):
+#     metrics_path: str
+#     webhook_url: Optional[str] = None
+
+# class OutputModel(BaseModel):
+#     message: str
+#     notification_sent: bool
+#     recipients: str
